ad9ef97872c0941e19cd2eb5384eb9b305daf3a9
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-20 20:56:38 -0700

    add PhotoListItem component, displays photo and user info on homepage

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/PhotoListItem.scss";
+console.log("Rendered PhotoListItem");
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
-};
+const PhotoListItem = (props) => {
+  const { id, location, imageSource, username, profile } = props;
+  
+  return (
+    <div className="photoListItem">
+      <img src={imageSource} alt="Photo" className="photo" />
+      <div className="profile-photo">
+        <img src={profile} alt="profile-photo" className="profile-photo" />
+      </div>
+      <div className="user-info">
+        <div>
+          <span className="username">{username}</span>
+        </div>
+        <div>
+          <span className="location">{location.city}, {location.country}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default PhotoListItem;
