6df803e343288c8e8cd668b131be4aaa02952d01
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-21 20:38:29 -0700

    add logic to display three photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,46 @@ import React from 'react';
  import PhotoList from './components/PhotoList';
  import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
+ const sampleDataForPhotoListItems = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+  {
+    id: "2",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  },
+  {
+    id: "3",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  },
+];
 
- const App = () => (
-   <div className="App">
-     <PhotoListItem photo={sampleDataForPhotoListItem} />
-   </div>
- )
+
+const App = () => (
+  <div className="App">
+    {sampleDataForPhotoListItems.map(photo => (
+      <PhotoListItem key={photo.id} photo={photo} />
+    ))}
+  </div>
+);
 
 export default App
