9da7e1b998faf74bd64aacdaf03282f0da0dd61e
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-22 20:29:00 -0700

    add onClick to like photo

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,24 @@
+// PhotoFavButton.jsx
 import React, { useCallback, useState } from 'react';
-
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  // Set initial state to un-liked
+  const [like, setLike] = useState(false);
+
+  // Set state to opposite of the previous state when icon is clicked
+  const handleClick = () => {
+    setLike(prevLike => !prevLike);
+  };
+
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+    // Clickable area for the favorite icon
+    <div className={"photo-list__fav-icon"} onClick={handleClick}>
+      {/* Actual favorite icon */}
+      <div className={"photo-list__fav-icon-svg"}>
+        <FavIcon selected={like} /> {/* Passes the like state as a prop to FavIcon */}
       </div>
     </div>
   );
