49196ed1d8a10109d63cbe93b1e73c6d789385aa
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-24 10:41:59 -0700

    refactor with mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,32 +1,15 @@
 // TopicList.jsx
 import React from "react";
-import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import "../styles/TopicList.scss";
 
-// Define topics array within TopicList.jsx
-const topics = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
-const TopicList = () => {
-  // Map through the topics array defined in this file
-  const parsedTopics = topics.map((topic) => (
+const TopicList = ({ topics }) => {
+  console.log("Topics:", topics);
+
+  const parsedTopics = topics ? topics.map((topic) => (
     <TopicListItem key={topic.id} label={topic.title} />
-  ));
+  )) : null;
 
   return <div className="top-nav-bar__topic-list">{parsedTopics}</div>;
 };
