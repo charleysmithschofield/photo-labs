c903e5352405e40cc65be5f21470dccafbc6421a
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-23 22:05:16 -0700

    add HomeRoute component with TopNavigationBar and PhotoList

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,17 @@
+// HomeRoute.jsx
 import React from 'react';
+import TopNavigationBar from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
 
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar />
+      <div>
+        <PhotoList />
+      </div>
     </div>
   );
 };
