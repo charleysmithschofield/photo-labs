fe5d53a66941002a56fb88edd3230c661f97e73c
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-24 16:02:21 -0700

    add close button

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,33 +1,26 @@
-// PhotoList.jsx
 import React from 'react';
 import PhotoListItem from './PhotoListItem';
-import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
-import '../styles/PhotoList.scss'
-
+import '../styles/PhotoList.scss';
 
 const PhotoList = ({ photos, toggleLike, likedPhotos, setDisplayModal }) => {
-
-  // handle image click
   const handleImageClick = () => {
     setDisplayModal(true);
-  }
+  };
+
   return (
     <ul className="photo-list">
-      {photos.map(photo => {
-        return (
-          <PhotoListItem
-            key={photo.id}
-            photo={photo}
-            toggleLike={toggleLike} 
-            likedPhotos={likedPhotos} 
-            onImageClick={handleImageClick}
-          />
-        );
-      })}
+      {photos.map(photo => (
+        <PhotoListItem
+          key={photo.id}
+          photo={photo}
+          toggleLike={toggleLike}
+          likedPhotos={likedPhotos}
+          onImageClick={handleImageClick}
+        />
+      ))}
     </ul>
   );
 };
 
-
-export default PhotoList
+export default PhotoList;
