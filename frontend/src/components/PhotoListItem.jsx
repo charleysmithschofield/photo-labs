18bbf4970be7570bbf44e36c9ff274f241504cb4
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-23 19:09:42 -0700

    change location.city and location.country into individual spans

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,30 +1,31 @@
 // PhotoListItem.jsx
 import React from 'react';
-import '../styles/PhotoListItem.scss'
+import '../styles/PhotoListItem.scss';
 import PhotoFavButton from './PhotoFavButton';
 
 console.log("Rendered PhotoListItem");
 
 const PhotoListItem = (props) => {
   const { location, urls, user } = props.photo;
-  
+
   return (
     <div className="photo-list__item">
       <PhotoFavButton />
-    <img src={urls.full} alt="Photo" className="photo-list__image" />
-    <div className="photo-list__user-details">
-      <img src={user.profile} alt="profile-photo" className="photo-list__user-profile" />
-      <div className="photo-list__user-info">
-        <div>
-          <span className="photo-list__username">{user.username}</span>
-        </div>
-        <div>
-          <span className="photo-list__user-location">{location.city}, {location.country}</span>
+      <img src={urls.full} alt="Photo" className="photo-list__image" />
+      <div className="photo-list__user-details">
+        <img src={user.profile} alt="profile-photo" className="photo-list__user-profile" />
+        <div className="photo-list__user-info">
+          <div>
+            <span className="photo-list__username">{user.username}</span>
+          </div>
+          <div className="photo-list__user-location">
+            <span> {location.city}</span>
+            <span> {location.country}</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
 export default PhotoListItem;
